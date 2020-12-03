aeed45e083d2924ed31b326055279a75bc061aef
Tom Schraitle <tom_schr@web.de>
2020-12-03 22:22:59 +0100

    Closure: make_averager

---
+++ topic-closures/averager.py
@@ -0,0 +1,22 @@
+"""
+
+"""
+
+
+def make_averager():
+    """
+    Creates an average
+
+    >>> avg = make_averager()
+    >>> avg(10)
+    10.0
+    >>> avg(15)
+    12.5
+    >>> avg(20)
+    15.0
+    """
+    series = []
+    def averager(new_value):
+        series.append(new_value)
+        return sum(series)/len(series)
+    return averager
