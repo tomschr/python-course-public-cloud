c7d3c7b80dce43946de6bf8ca2d7494d887a4070
Tom Schraitle <tom_schr@web.de>
2021-02-17 23:03:00 +0100

    Consolidate topic-generator
    Move all content to topic-generators (plural)

--- topic-generators/countdown.py
+++ topic-generators/countdown.py
@@ -1,7 +1,10 @@
 def countdown(n):
-    while n > 0:
+    """
+    Countdown from the start value n to zero.
+
+    :param n: the integer to start from
+    :yield: the current value
+    """
+    while n >= 0:
         yield n
         n -= 1
-
-for i in countdown(5):
-    print(i)
